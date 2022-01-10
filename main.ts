43ef3349defe2eff618dc64ca426681e5c9a2af4
nananaman <foolawoola@gmail.com>
2022-01-10 18:50:52 +0900

    update: use sift

--- main.ts
+++ main.ts
@@ -1,10 +1,41 @@
-import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { json, PathParams, serve } from "https://deno.land/x/sift@0.4.2/mod.ts";
 
-const addr = ":8080";
-console.log(`Listening on http://localhost${addr}`);
-serve((req: Request) => {
-  const { pathname } = new URL(req.url);
-  return new Response(`Hello World from ${pathname}!`, {
-    headers: { "content-type": "text/plain" },
-  });
+serve({
+  "/": () => new Response("hello world"),
+  "/books": () => {
+    return json(books);
+  },
+  "/books/:id": (_request: Request, params: PathParams) => {
+    if (params === undefined) {
+      return Response.error();
+    }
+    const book = books.find((book) => book.isbn === params.id);
+    if (book === undefined) {
+      return new Response(`Unknown id: ${params.id}`);
+    }
+    return json(book);
+  },
+  // The route handler of 404 will be invoked when a route handler
+  // for the requested path is not found.
+  404: () => new Response("not found"),
 });
+
+interface Book {
+  isbn: string;
+  author: string;
+  title: string;
+}
+
+const books: Array<Book> = [{
+  isbn: "1",
+  author: "Robin Wieruch",
+  title: "The Road to React",
+}, {
+  isbn: "2",
+  author: "Kyle Simpson",
+  title: "You Don't Know JS: Scope & Closures",
+}, {
+  isbn: "3",
+  author: "Andreas A. Antonopoulos",
+  title: "Mastering Bitcoin",
+}];
